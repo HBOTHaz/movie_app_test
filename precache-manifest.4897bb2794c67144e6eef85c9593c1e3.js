5a016f51f3124debcb7792d0bcc8857f6b9b1cc3
HBOTHaz <hazandme@naver.com>
2020-02-07 14:38:00 +0900

    Updates

---
+++ precache-manifest.4897bb2794c67144e6eef85c9593c1e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0c81fa698de9f64cd6f28ee448af891",
+    "url": "/movie_app_test/index.html"
+  },
+  {
+    "revision": "699949f69eacfcde8bfd",
+    "url": "/movie_app_test/static/css/main.985ef1d7.chunk.css"
+  },
+  {
+    "revision": "be04fe4cf119ca68882b",
+    "url": "/movie_app_test/static/js/2.82656079.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app_test/static/js/2.82656079.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "699949f69eacfcde8bfd",
+    "url": "/movie_app_test/static/js/main.1238e8d4.chunk.js"
+  },
+  {
+    "revision": "d3c1557c4aeb6a14717d",
+    "url": "/movie_app_test/static/js/runtime-main.4ba950a6.js"
+  }
+]);
